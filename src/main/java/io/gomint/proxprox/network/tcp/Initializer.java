b20748c460ffa10bd94682d720b5404278f1de26
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2018-09-21 08:53:03 +0200

    Implement high and low watermarks for write buffers

--- src/main/java/io/gomint/proxprox/network/tcp/Initializer.java
+++ src/main/java/io/gomint/proxprox/network/tcp/Initializer.java
@@ -3,16 +3,21 @@ package io.gomint.proxprox.network.tcp;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.gomint.proxprox.network.UpstreamConnection;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelException;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.WriteBufferWaterMark;
 import io.netty.channel.socket.SocketChannel;
 
 import java.util.function.Consumer;
 
 public class Initializer {
+
+    private static final int LOW_MARK = Integer.getInteger( "io.gomint.proxprox.low_mark", 2 << 18 );   // 0.5 mb
+    private static final int HIGH_MARK = Integer.getInteger( "io.gomint.proxprox.high_mark", 2 << 20 ); // 2 mb
+    private static final WriteBufferWaterMark MARK = new WriteBufferWaterMark( LOW_MARK, HIGH_MARK );
 
     private static final EventLoopGroup EVENT_LOOP_GROUP;
 
@@ -29,7 +34,14 @@ public class Initializer {
                 .handler( new ChannelInitializer<SocketChannel>() {
                               @Override
                               public void initChannel( SocketChannel ch ) throws Exception {
-                                  ch.config().setOption( ChannelOption.IP_TOS, 0x18 );
+                                  try {
+                                      ch.config().setOption( ChannelOption.IP_TOS, 0x18 );
+                                  } catch ( ChannelException e ) {
+                                      // Ignore
+                                  }
+
+                                  ch.config().setAllocator( PooledByteBufAllocator.DEFAULT );
+                                  ch.config().setWriteBufferWaterMark( MARK );
                                   Pipeline.prepare( ch.pipeline(), connectionHandler );
                               }
                           }
